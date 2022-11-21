60a513e4f6f2af3c6f950701d7b19f82ac1c6873
Hyrondelle <n.hyr@free.fr>
2022-11-21 22:12:26 +0100

    troisieme

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+    const [post,setPost] = useState<string>('post');
+    
+    const Submit = async() =>{
+        const postObj = {title:post}
+        await axios.post('http://localhost:3000/post',postObj)
+        .then((res)=>console.log(res))
+        .catch((e)=>console.log(e))
+    }
+    useEffect(()=>{
+        axios.get('http://localhost:3000/posts')
+        .then((res)=>console.log(res))
+        .catch((e)=>console.log(e))
+    },[])
     return (
         <div className='home'>
             <h1>coucou</h1>
+            <label htmlFor="post">message:</label>
+            <input onChange={(e)=>setPost(e.target.value)} type="text" name="post" id="post" />
+            <button onClick={Submit} type="submit">Envoyer</button>
         </div>
     );
 };

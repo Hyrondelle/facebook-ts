a7bb4ccc4a7fe142f9ad5ce856154af0f55a1c90
hyrondelle <n.hyr@free.fr>
2022-11-30 15:56:36 +0100

    orga connection

--- src/pages/Connection.tsx
+++ src/pages/Connection.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Connection = () => {
     return (
-        <div>
-            connexion
+        <div className='connection'>
+            <div className='container'>
+                <div className='signup'>
+
+                </div>
+                <div className='trait'>
+
+                </div>
+                <div className='login'>
+
+                </div>
+            </div>
         </div>
     );
 };

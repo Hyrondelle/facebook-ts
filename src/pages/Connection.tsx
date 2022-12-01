1ee2ad4d014dd69dea9192c2352df2452889ebee
hyrondelle <n.hyr@free.fr>
2022-12-01 15:58:19 +0100

    style

--- src/pages/Connection.tsx
+++ src/pages/Connection.tsx
@@ -5,13 +5,25 @@ const Connection = () => {
         <div className='connection'>
             <div className='container'>
                 <div className='signup'>
-
+                    <form >
+                        <label htmlFor="email">Email:</label>
+                        <input type="text" name="email" id="email" />
+                        <label htmlFor="mdp">Mot de passe:</label>
+                        <input type="text" name="mdp" id="mdp" />
+                        <label htmlFor="confirm">confirmer:</label>
+                        <input type="text" name="confirm" id="confirm" />
+                    </form>
                 </div>
                 <div className='trait'>
 
                 </div>
                 <div className='login'>
-
+                <form >
+                        <label htmlFor="email">Email:</label>
+                        <input type="text" name="email" id="email" />
+                        <label htmlFor="mdp">Mot de passe:</label>
+                        <input type="text" name="mdp" id="mdp" />
+                    </form>
                 </div>
             </div>
         </div>

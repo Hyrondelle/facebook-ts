233b003ec477fe0d0c0b0d42f75d471b4b6684fa
hyrondelle <n.hyr@free.fr>
2022-12-22 16:40:22 +0100

    style post

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FaPen} from 'react-icons/fa'
 
 const Post = (props:any) => {
     return (
@@ -6,17 +7,20 @@ const Post = (props:any) => {
             <div className='post-contain'>
                 <p>{props.post.message}</p>
             </div>
-            <div className="total">
-                <div className='like btn'>like</div>
-                <div>
-                    <div className='comment btn'>comment</div>
-                    <div className='partage btn'>partage</div>
+            <div className="social">
+                <div className="total">
+                    <div className='like btn'>nblike</div>
+                    <div className='btn_x2'>
+                        <div className='comment btn'>nbcomment</div>
+                        <div className='partage btn'>nbpartage</div>
+                    </div>
                 </div>
-            </div>
-            <div className='buttons'>
-                <div className='like btn'>like</div>
-                <div className='comment btn'>comment</div>
-                <div className='partage btn'>partage</div>
+                <div className='buttons'>
+                    <div className='like btn centre'>like</div>
+                    <div className='comment btn centre'>comment</div>
+                    <div className='partage btn centre'>partage</div>
+                    <div className='modify'><FaPen/></div>
+                </div>
             </div>
         </div>
     );

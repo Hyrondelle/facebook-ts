4233ca4c21e1abce63ce629677a0d2f75c831956
hyrondelle <n.hyr@free.fr>
2023-01-18 17:21:17 +0100

    update post

--- src/components/Update.tsx
+++ src/components/Update.tsx
@@ -7,11 +7,16 @@ const Update = (props:any) => {
     const toggleStyleBtn = () =>{
         click.setClick(false);
     }
-    
+    const submitChange = async() =>{
+        await fetch('http://localhost:3000/updateposts/:'+props.id,{method: 'POST'})
+        .then((res)=>console.log(res))
+        .catch((e)=>console.log(e))
+    }
      return (
          <div className='update' id='updateEl'>
              
              <input autoFocus type="text" name="update" id="update" defaultValue={props.affichePost}/>
+             <button onClick={submitChange} type="submit">modifier</button>
              <button onClick={toggleStyleBtn}>X</button>
          </div>
      );

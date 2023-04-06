cd4a8a5346297eff00b75245bd6ec3075f81c28b
hyrondelle <n.hyr@free.fr>
2023-04-06 15:58:07 +0200

    maj

--- back/routes/user.routes.js
+++ back/routes/user.routes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
+const multer = require('multer');
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
 const uploadController = require('../controllers/upload.controller');
-const multer = require("multer");
 const upload = multer();
 
 // auth
